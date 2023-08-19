50b46e5dca072cfc67e8b6c5f44b7c286b0b76d9
Juan José Rodriguez <zoranbow@gmail.com>
2023-08-19 08:22:07 -0500

    feat(api/serve favicon.ico)

--- api/src/app.js
+++ api/src/app.js
@@ -2,6 +2,8 @@ const { CLIENT_URL } = process.env;
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const favicon = require('serve-favicon');
+const path = require('path');
 const routes = require('./routes');
 
 const server = express();
@@ -9,6 +11,7 @@ const server = express();
 server.use(express.json());
 server.use(morgan('dev'));
 server.use(cors({ origin: CLIENT_URL }));
+server.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 server.use('/api', routes);
 
