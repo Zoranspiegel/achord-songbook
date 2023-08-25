68660db3559ec2ad78101b7d6ec3733385d85e14
Juan José Rodriguez <zoranbow@gmail.com>
2023-08-25 18:06:01 -0500

    feat(api/cloudinary user profile_picture store)

--- api/index.js
+++ api/index.js
@@ -1,10 +1,22 @@
 require('dotenv').config();
 const app = require('./src/app');
 const { db } = require('./src/db');
+const { v2 } = require('cloudinary');
+const {
+  CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET
+} = process.env;
 
 const PORT = process.env.PORT || 3001;
 
-db.sync({ force: true })
+v2.config({
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET
+});
+
+db.sync({ force: false })
   .then(() => {
     console.log('🐘 Connected to postgreSQL database');
     app.listen(PORT, () => {
--- api/src/controllers/logUser.js
+++ api/src/controllers/logUser.js
@@ -1,22 +1,59 @@
 const jwt = require('jsonwebtoken');
+const { v2 } = require('cloudinary');
 const { JWT_SECRET } = process.env;
 const { user } = require('../db');
 
+/* eslint-disable */
+
 module.exports = async (req, res) => {
   try {
-    const { auth0_sub, ...rest } = req.body;
-    const [loggedUser, created] = await user.findOrCreate({
-      where: {
-        auth0_sub
-      },
-      defaults: rest
-    });
-    const statusCode = created ? 201 : 200;
-    jwt.sign({ ...loggedUser.dataValues }, JWT_SECRET, (error, token) => {
-      if (error) return res.sendStatus(403);
-      res.status(statusCode).json({ ...loggedUser.dataValues, token });
-    });
+    const { auth0_sub, profile_picture, ...rest } = req.body;
+    const foundUsers = await user.findAll({ where: { auth0_sub } });
+    if (!foundUsers.length) {
+      let cloudinary_profile_picture;
+      await v2.uploader.upload(
+        profile_picture,
+        { public_id: auth0_sub, folder: 'achord/profile_pictures' },
+        (error, picture) => {
+          if (error) return console.log('Unable to upload profile_picture');
+          if (picture) {
+            console.log(cloudinary_profile_picture);
+            console.log(picture.secure_url);
+            cloudinary_profile_picture = picture.secure_url;
+          }
+        }
+      );
+      const newUser = await user.create({ ...rest, profile_picture: cloudinary_profile_picture, auth0_sub });
+      jwt.sign({ id: newUser.id }, JWT_SECRET, (error, token) => {
+        if (error) return res.sendStatus(403);
+        res.status(201).json({ ...newUser.dataValues, token });
+      });
+    } else {
+      jwt.sign({ ...foundUsers[0] }, JWT_SECRET, (error, token) => {
+        if (error) return res.sendStatus(403);
+        res.status(200).json({ ...foundUsers[0].dataValues, token });
+      });
+    }
   } catch (error) {
-    res.status(404).json({ error: error.message });
+    res.status(400).json({ error: error.message });
   }
 };
+
+// module.exports = async (req, res) => {
+//   try {
+//     const { auth0_sub, ...rest } = req.body;
+//     const [loggedUser, created] = await user.findOrCreate({
+//       where: {
+//         auth0_sub
+//       },
+//       defaults: rest
+//     });
+//     const statusCode = created ? 201 : 200;
+//     jwt.sign({ ...loggedUser.dataValues }, JWT_SECRET, (error, token) => {
+//       if (error) return res.sendStatus(403);
+//       res.status(statusCode).json({ ...loggedUser.dataValues, token });
+//     });
+//   } catch (error) {
+//     res.status(404).json({ error: error.message });
+//   }
+// };
