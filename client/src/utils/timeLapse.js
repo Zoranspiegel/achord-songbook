190f9a984ab24c22ae653fbe7eaa0d8c819b26ee
Juan José Rodriguez <zoranbow@gmail.com>
2023-08-25 18:37:23 -0500

    fix(client/styles minor fixes)

--- client/src/utils/timeLapse.js
+++ client/src/utils/timeLapse.js
@@ -1,6 +1,5 @@
 export default function timeLapse(date) {
   const myDate = new Date(date);
   const actualDate = new Date();
-  console.log(Number(actualDate) - Number(myDate));
   return Number(actualDate) - Number(myDate);
 }
