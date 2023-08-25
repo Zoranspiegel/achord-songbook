6a74e2d097380164302617055933e796b8f14439
Juan José Rodriguez <zoranbow@gmail.com>
2023-08-25 18:19:02 -0500

    feat(client/react-parallax-tilt effect on cards)

--- client/src/components/SongCard.jsx
+++ client/src/components/SongCard.jsx
@@ -1,15 +1,21 @@
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
+import Tilt from 'react-parallax-tilt';
 import style from './styles/SongCard.module.css';
 
 export default function SongCard({ song }) {
   return (
-    <NavLink to={`/song/details/${song.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-      <div className={style.card__container}>
-        <h1 className={style.card__title}>{song.title}</h1>
-        <h2 className={style.card__artist}>{song.artist}</h2>
-        <p className={style.card__content}>{song.content.slice(0, 20)}...</p>
-      </div>
+    <NavLink
+      to={`/song/details/${song.id}`}
+      style={{ textDecoration: 'none', color: 'inherit' }}
+    >
+      <Tilt glareEnable='true' glareBorderRadius='10px'>
+        <div className={style.card__container}>
+          <h1 className={style.card__title}>{song.title}</h1>
+          <h2 className={style.card__artist}>{song.artist}</h2>
+          <p className={style.card__content}>{song.content.slice(0, 20)}...</p>
+        </div>
+      </Tilt>
     </NavLink>
   );
 }
